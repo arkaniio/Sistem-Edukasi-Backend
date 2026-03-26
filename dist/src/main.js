9b65e6ee16d64d59dfc71ae33a2a5e4263e36929
arkaniio <laluahmadarkani@gmail.com>
2026-03-26 21:10:38 +0800

    change bug

--- dist/src/main.js
+++ dist/src/main.js
@@ -5,7 +5,8 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
-    await app.listen(process.env.PORT ?? 9000);
+    app.setGlobalPrefix('api');
+    await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
